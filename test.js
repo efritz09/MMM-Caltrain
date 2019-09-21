3593bef3ac94914303abee0927dcb6e2b5b50ace
eric <efritz09@gmail.com>
2019-09-21 16:04:02 -0700

    working delayed train check

--- test.js
+++ test.js
@@ -1,35 +1,60 @@
 // run using node test.js
 // npm install request
 
+// vars to be added later:
+// - key
+// - delay threshold
+// - stops to monitor (how many?)
+// stretch:
+// - specific trains
+
 const request = require("request");
 const zlib = require('zlib');
+const http = require('http')
 const BASE_URL = 'http://api.511.org/transit/';
 
-console.log("checkpoint! 0");
+const delay_time = 5000; // milliseconds
+// const delay_time = 10000; // milliseconds
 
-function CheckForDelaysCallback(data) {
+function SomeCallbackHere(data) {
 	console.log(data);
 }
 
-function gunzip(raw, callback) {
-	console.log("gunzipping");
-	zlib.gunzip(raw, function(err, result) {
-		if (err) {
-			console.log("Error gunzipping: ", err);
-		} else {
-			console.log("checkpoint! 4");
-			callback(result.toString());
+// compare the aimed arrival and expected arrival times to find delays
+function CheckForDelaysCallback(raw_json) {
+	var delayed_trains = [];
+	json = JSON.parse(raw_json)
+	data = json.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit
+	for (var i = 0, len = data.length; i < len; i++) {
+		train = data[i].MonitoredVehicleJourney
+		call = train.MonitoredCall
+		arrive = Date.parse(call.AimedArrivalTime)
+		exp = Date.parse(call.ExpectedArrivalTime)
+
+		if ((arrive - exp) > delay_time) {
+			delayed_trains.push({
+				train: train.VehicleRef,
+				stop: call.StopPointName,
+				dir: train.DirectionRef,
+				delay: (arrive - exp) / 1000,
+			})
 		}
-	});
+	}
+
+	SomeCallbackHere(delayed_trains);
 }
 
 function getRequest(options, callback) {
 	request(options, function(err, resp, body) {
 		if(!err && resp.statusCode == 200) {
 			if(resp.headers['content-encoding'] == 'gzip') {
-				console.log("gunzipping")
-				data = gunzip(body, callback);
-				console.log("checkpoint! 3");
+				zlib.gunzip(body, function(err, result) {
+					if (err) {
+						console.log("Error gunzipping: ", err);
+					} else {
+						callback(result.toString("utf-8").trim());
+					}
+				})
 			}
 		} else {
 			console.log("request error: ", err, resp.statusCode);
@@ -47,11 +72,12 @@ function CheckForDelays() {
 	        'agency': 'CT',
 	        'stopCode': '',
 	        'api_key': 'fa666f48-2174-4618-a349-97390b7e3e4d',
+	    },
+	    headers: {
+	    	'Content-Type': 'application/json',
 	    }
 	}
-	console.log("checkpoint! 1");
 	data = getRequest(options, CheckForDelaysCallback);
-	console.log("checkpoint! 2");
 }
 
 CheckForDelays();
