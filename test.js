fd6e93e32a6071e1ab9232db3fb3e877030187e6
eric <efritz09@gmail.com>
2019-09-21 17:21:56 -0700

    station queries. added caltrain station codes

--- test.js
+++ test.js
@@ -8,21 +8,21 @@
 // stretch:
 // - specific trains
 
-const request = require("request");
-const zlib = require('zlib');
+const request = require("request")
+const zlib = require('zlib')
 const http = require('http')
-const BASE_URL = 'http://api.511.org/transit/';
+const BASE_URL = 'http://api.511.org/transit/'
 
-const delay_time = 5000; // milliseconds
-// const delay_time = 10000; // milliseconds
+const delay_time = 5000 // milliseconds
+// const delay_time = 10000 // milliseconds
 
 function SomeCallbackHere(data) {
-	console.log(data);
+	console.log(data)
 }
 
 // compare the aimed arrival and expected arrival times to find delays
 function CheckForDelaysCallback(raw_json) {
-	var delayed_trains = [];
+	var delayed_trains = []
 	json = JSON.parse(raw_json)
 	data = json.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit
 	for (var i = 0, len = data.length; i < len; i++) {
@@ -31,17 +31,40 @@ function CheckForDelaysCallback(raw_json) {
 		arrive = Date.parse(call.AimedArrivalTime)
 		exp = Date.parse(call.ExpectedArrivalTime)
 
-		if ((arrive - exp) > delay_time) {
+		if ((exp - arrive) > delay_time) {
 			delayed_trains.push({
 				train: train.VehicleRef,
 				stop: call.StopPointName,
 				dir: train.DirectionRef,
-				delay: (arrive - exp) / 1000,
+				delay: (exp - arrive) / 1000 / 60,
 			})
 		}
 	}
+	console.log("CheckForDelaysCallback")
+	SomeCallbackHere(delayed_trains)
+}
 
-	SomeCallbackHere(delayed_trains);
+// returns all reported train statuses for a given station
+function GetStationStatusCallback(raw_json) {
+	station_status = []
+	json = JSON.parse(raw_json)
+	data = json.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit
+	for (var i = 0, len = data.length; i < len; i++) {
+		train = data[i].MonitoredVehicleJourney
+		call = train.MonitoredCall
+		arrive = Date.parse(call.AimedArrivalTime)
+		exp = Date.parse(call.ExpectedArrivalTime)
+		status = (exp - arrive) / 1000 / 60
+
+		station_status.push({
+			train: train.VehicleRef,
+			delay: status,
+			dir: train.DirectionRef,
+		})
+	}
+
+	console.log("GetStationStatusCallback")
+	SomeCallbackHere(station_status)
 }
 
 function getRequest(options, callback) {
@@ -50,14 +73,14 @@ function getRequest(options, callback) {
 			if(resp.headers['content-encoding'] == 'gzip') {
 				zlib.gunzip(body, function(err, result) {
 					if (err) {
-						console.log("Error gunzipping: ", err);
+						console.log("Error gunzipping: ", err)
 					} else {
-						callback(result.toString("utf-8").trim());
+						callback(result.toString("utf-8").trim())
 					}
 				})
 			}
 		} else {
-			console.log("request error: ", err, resp.statusCode);
+			console.log("request error: ", err, resp.statusCode)
 		}
 	})
 }
@@ -77,7 +100,28 @@ function CheckForDelays() {
 	    	'Content-Type': 'application/json',
 	    }
 	}
-	data = getRequest(options, CheckForDelaysCallback);
+	data = getRequest(options, CheckForDelaysCallback)
 }
 
-CheckForDelays();
+function GetStationStatus() {
+	// This can be inaccurate if the train is not set to arrive soon. It may be
+	// good to threshold this somewhere. Perhaps list the upcomming trains but
+	// don't display the status until it's close to the station
+	options = {
+	    url: BASE_URL + 'StopMonitoring',
+	    method: 'GET',
+	    encoding: null,
+	    qs: {
+	        'agency': 'CT',
+	        'stopCode': '70112',
+	        'api_key': 'fa666f48-2174-4618-a349-97390b7e3e4d',
+	    },
+	    headers: {
+	    	'Content-Type': 'application/json',
+	    }
+	}
+	data = getRequest(options, GetStationStatusCallback)
+}
+
+CheckForDelays()
+GetStationStatus()
