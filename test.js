40794f57b21da6d8fe96753a815a12cd37654466
eric <efritz09@gmail.com>
2019-09-22 23:22:49 -0700

    update node helper, update test to be more similar to node_helper

--- test.js
+++ test.js
@@ -13,11 +13,55 @@ const zlib = require('zlib')
 const http = require('http')
 const BASE_URL = 'http://api.511.org/transit/'
 
-const delay_time = 5000 // milliseconds
+const delay_time = 50 // milliseconds
 // const delay_time = 10000 // milliseconds
 
 function SomeCallbackHere(data) {
 	console.log(data)
+}
+
+function run(query, parameters) {
+    var options
+    if(query === "CheckForDelays") {
+        options = BuildCheckForDelays(parameters)
+        // CheckForDelays
+
+        // self.CheckForDelays(query, parameters)
+    } else if(query === "GetStationStatus") {
+        options = BuildGetStationStatus(parameters)
+        // self.GetStationStatus(query, parameters)
+    }
+    console.log(options)
+
+	request(options, function(err, resp, body) {
+		if(!err && resp.statusCode == 200) {
+			if(resp.headers['content-encoding'] == 'gzip') {
+				zlib.gunzip(body, function(err, result) {
+					if (err) {
+						console.log("Error gunzipping: ", err)
+					} else {
+						console.log()
+						callback(query, result.toString("utf-8").trim())
+					}
+				})
+			}
+		} else {
+			console.log("request error: ", err, resp.statusCode)
+		}
+	})
+}
+
+function callback(query, raw_json) {
+	console.log(raw_json)
+    if(query === "CheckForDelays") {
+        CheckForDelaysCallback(raw_json)
+        // CheckForDelays
+
+        // self.CheckForDelays(query, parameters)
+    } else if(query === "GetStationStatus") {
+        GetStationStatusCallback(raw_json)
+        // self.GetStationStatus(query, parameters)
+    }
 }
 
 // compare the aimed arrival and expected arrival times to find delays
@@ -28,6 +72,7 @@ function CheckForDelaysCallback(raw_json) {
 	for (var i = 0, len = data.length; i < len; i++) {
 		train = data[i].MonitoredVehicleJourney
 		call = train.MonitoredCall
+		console.log(train)
 		arrive = Date.parse(call.AimedArrivalTime)
 		exp = Date.parse(call.ExpectedArrivalTime)
 
@@ -67,26 +112,7 @@ function GetStationStatusCallback(raw_json) {
 	SomeCallbackHere(station_status)
 }
 
-function getRequest(options, callback) {
-	request(options, function(err, resp, body) {
-		if(!err && resp.statusCode == 200) {
-			if(resp.headers['content-encoding'] == 'gzip') {
-				zlib.gunzip(body, function(err, result) {
-					if (err) {
-						console.log("Error gunzipping: ", err)
-					} else {
-						callback(result.toString("utf-8").trim())
-					}
-				})
-			}
-		} else {
-			console.log("request error: ", err, resp.statusCode)
-		}
-	})
-}
-
-
-function CheckForDelays() {
+function BuildCheckForDelays() {
 	options = {
 	    url: BASE_URL + 'StopMonitoring',
 	    method: 'GET',
@@ -100,10 +126,10 @@ function CheckForDelays() {
 	    	'Content-Type': 'application/json',
 	    }
 	}
-	data = getRequest(options, CheckForDelaysCallback)
+	return options
 }
 
-function GetStationStatus() {
+function BuildGetStationStatus() {
 	// This can be inaccurate if the train is not set to arrive soon. It may be
 	// good to threshold this somewhere. Perhaps list the upcomming trains but
 	// don't display the status until it's close to the station
@@ -120,8 +146,10 @@ function GetStationStatus() {
 	    	'Content-Type': 'application/json',
 	    }
 	}
-	data = getRequest(options, GetStationStatusCallback)
+	return options
 }
 
-CheckForDelays()
-GetStationStatus()
+run("CheckForDelays", "")
+// run("GetStationStatus", "")
+// CheckForDelays()
+// GetStationStatus()
