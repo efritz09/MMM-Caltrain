cf9fcb41c834d1432d1b05314cffcd77aa8adcce
eric <efritz09@gmail.com>
2019-09-22 23:22:49 -0700

    update node helper, update test to be more similar to node_helper

--- node_helper.js
+++ node_helper.js
@@ -4,6 +4,9 @@ var zlib = require('zlib')
 
 const BASE_URL = 'http://api.511.org/transit/'
 
+// TODO: abstract this
+const delay_time = 5000 // milliseconds
+
 module.exports = NodeHelper.create({
     start: function() {
         console.log("Starting node helper: " + this.name);
@@ -12,19 +15,44 @@ module.exports = NodeHelper.create({
     socketNotificationReceived: function(query, parameters) {
         var self = this
         console.log("Query: " + query + " Parameters: " + parameters)
-
+        var options
         if(query === "CheckForDelays") {
-            self.CheckForDelays(parameters)
+            options = self.buildCheckForDelays(parameters)
         } else if(query === "GetStationStatus") {
-            self.GetStationStatus(parameters)
+            options = self.buildGetStationStatus(parameters)
         }
+
+        request(options, function(err, resp, body) {
+            if(!err && resp.statusCode == 200) {
+                if(resp.headers['content-encoding'] == 'gzip') {
+                    zlib.gunzip(body, function(err, result) {
+                        if (err) {
+                            console.log("Error gunzipping: ", err)
+                        } else {
+                            requestHandler(query, result.toString("utf-8").trim())
+                        }
+                    })
+                }
+            } else {
+                console.log("request error: ", err, resp.statusCode)
+            }
+        })
     },
 
+    requestHandler(query, data) {
+        if(query === "CheckForDelays") {
+            options = self.checkForDelaysCallback(data)
+        } else if(query === "GetStationStatus") {
+            options = self.getStationStatusCallback(data)
+        }
+
+    }
+
     // compare the aimed arrival and expected arrival times to find delays
-    CheckForDelaysCallback: function(raw_json) {
+    checkForDelaysCallback: function(data) {
         var self = this
         var delayed_trains = []
-        json = JSON.parse(raw_json)
+        json = JSON.parse(data)
         data = json.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit
         for (var i = 0, len = data.length; i < len; i++) {
             train = data[i].MonitoredVehicleJourney
@@ -46,10 +74,10 @@ module.exports = NodeHelper.create({
     },
 
     // returns all reported train statuses for a given station
-    GetStationStatusCallback: function(raw_json) {
+    getStationStatusCallback: function(data) {
         var self = this
         station_status = []
-        json = JSON.parse(raw_json)
+        json = JSON.parse(data)
         data = json.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit
         for (var i = 0, len = data.length; i < len; i++) {
             train = data[i].MonitoredVehicleJourney
@@ -69,93 +97,37 @@ module.exports = NodeHelper.create({
         self.sendSocketNotification("GetStationStatus", station_status);
     },
 
-    // getRequest: function(options, callback) {
-    //     var self = this
-    //     request(options, function(err, resp, body) {
-    //         if(!err && resp.statusCode == 200) {
-    //             if(resp.headers['content-encoding'] == 'gzip') {
-    //                 zlib.gunzip(body, function(err, result) {
-    //                     if (err) {
-    //                         console.log("Error gunzipping: ", err)
-    //                     } else {
-    //                         callback(result.toString("utf-8").trim())
-    //                     }
-    //                 })
-    //             }
-    //         } else {
-    //             console.log("request error: ", err, resp.statusCode)
-    //         }
-    //     })
-    // },
-
-    CheckForDelays: function(parameters) {
-        var self = this
-        options = {
+    buildCheckForDelays: function(parameters) {
+        return options = {
             url: BASE_URL + 'StopMonitoring',
             method: 'GET',
             encoding: null,
             qs: {
                 'agency': 'CT',
-                'stopCode': '',
-                'api_key': 'fa666f48-2174-4618-a349-97390b7e3e4d',
+                'api_key': 'fa666f48-2174-4618-a349-97390b7e3e4d', // TODO: abstract this in parameters
             },
             headers: {
                 'Content-Type': 'application/json',
             }
         }
-
-        request(options, function(err, resp, body) {
-            if(!err && resp.statusCode == 200) {
-                if(resp.headers['content-encoding'] == 'gzip') {
-                    zlib.gunzip(body, function(err, result) {
-                        if (err) {
-                            console.log("Error gunzipping: ", err)
-                        } else {
-                            self.CheckForDelaysCallback(result.toString("utf-8").trim())
-                        }
-                    })
-                }
-            } else {
-                console.log("request error: ", err, resp.statusCode)
-            }
-        })
-        // data = self.getRequest(options, )
     },
 
-    GetStationStatus: function(parameters) {
+    buildGetStationStatus: function(parameters) {
         // This can be inaccurate if the train is not set to arrive soon. It may be
         // good to threshold this somewhere. Perhaps list the upcomming trains but
         // don't display the status until it's close to the station
-        var self = this
-        options = {
+        return options = {
             url: BASE_URL + 'StopMonitoring',
             method: 'GET',
             encoding: null,
             qs: {
                 'agency': 'CT',
                 'stopCode': '70112',
-                'api_key': 'fa666f48-2174-4618-a349-97390b7e3e4d',
+                'api_key': 'fa666f48-2174-4618-a349-97390b7e3e4d', // TODO: abstract this in parameters
             },
             headers: {
                 'Content-Type': 'application/json',
             }
         }
-        
-        request(options, function(err, resp, body) {
-            if(!err && resp.statusCode == 200) {
-                if(resp.headers['content-encoding'] == 'gzip') {
-                    zlib.gunzip(body, function(err, result) {
-                        if (err) {
-                            console.log("Error gunzipping: ", err)
-                        } else {
-                            self.GetStationStatusCallback(result.toString("utf-8").trim())
-                        }
-                    })
-                }
-            } else {
-                console.log("request error: ", err, resp.statusCode)
-            }
-        })
-        // data = self.getRequest(options, self.GetStationStatusCallback)
     },
 })
