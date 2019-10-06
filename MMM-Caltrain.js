d336c993fdb67e03b3142c55c96907f334f6aa75
Eric Fritz <efritz09@gmail.com>
2019-10-06 12:44:09 -0700

    First version (#2)
    * working station reporting and delay monitoring

---
+++ MMM-Caltrain.js
@@ -0,0 +1,267 @@
+
+Module.register("MMM-Caltrain", {
+
+    // Default config
+    defaults: {
+        text: "Caltrain Monitor",
+        delayThreshold: 600000, // 10 minutes
+        direction: "", // if unset, both directions
+        requestDelays: true,
+        showDelayedTrains: true, // if true, shows which trains are delayed
+        showDelayWarning: true, // if true, shows a warning when trains are delayd
+        timeFormat: 24, // default 24 hour time
+        updateInterval: 180000, // 3 minutes
+    },
+
+    start: function() {
+        Log.info("starting module: " + this.name);
+        var self = this;
+
+        this.loaded = false;
+        this.delays = [];
+        this.stationNorth = [];
+        this.stationSouth = [];
+
+        if (self.config.requestDelays) {
+            self.getDelayInfo();            
+        }
+        self.getStationInfo();
+
+        // Schedule update timer.
+        setInterval(function() {
+            if (self.config.requestDelays) {
+                self.getDelayInfo();            
+            }
+            self.getStationInfo();
+        }, self.config.updateInterval);
+    },
+
+    getStyles: function() {
+        return ["MMM-Caltrain.css"];
+    },
+
+    getHeader: function() {
+        return "Caltrain Departure Time: " + this.config.stationName;
+    },
+
+    // Requests information on delayed trains across the fleet
+    getDelayInfo: function() {
+        Log.info("Requesting delay info");
+        this.sendSocketNotification("CheckForDelays", this.config);
+    },
+
+    // Requests information for trains arriving at a given station
+    // If no direction is chose, both directions are reported
+    getStationInfo: function() {
+        Log.info("Requesting station info");
+        if (this.config.direction.toLowerCase() === "south") {
+            this.getSouthboundTrains();
+        } else if (this.config.direction.toLowerCase() === "north") {
+            this.getNorthboundTrains();
+        } else {
+            this.getNorthboundTrains();
+            this.getSouthboundTrains();
+        }
+    },
+
+    // Requests southbound train information
+    getSouthboundTrains: function() {
+        Log.info("Requesting southbound info");
+        var params = {
+            key: this.config.key,
+            trainDirection: "south",
+            stationName: this.config.stationName,
+        }
+        this.sendSocketNotification("GetStationStatus", params);
+    },
+
+    // Requests northbound train information
+    getNorthboundTrains: function() {
+        Log.info("Requesting northbound info");
+        var params = {
+            key: this.config.key,
+            trainDirection: "north",
+            stationName: this.config.stationName,
+        }
+        this.sendSocketNotification("GetStationStatus", params);
+    },
+
+    // Creates the station train table
+    createTrainTable: function(trains) {
+        var table = document.createElement("table");
+        table.className = "small";
+
+        table.appendChild(document.createElement("thead"));
+        var head = document.createElement("tr");
+        head.className = "scheduleHeader"
+        table.appendChild(head);
+        
+        var trainNum = document.createElement("th");
+        trainNum.innerHTML = "Train";
+        var trainLine = document.createElement("th");
+        trainLine.innerHTML = "Line";
+        var trainETA = document.createElement("th");
+        trainETA.innerHTML = "ETA";
+        var trainStatus = document.createElement("th");
+        trainStatus.innerHTML = "Status";
+
+        head.appendChild(trainNum);
+        head.appendChild(trainLine);
+        head.appendChild(trainETA);
+        head.appendChild(trainStatus);
+
+        var body = document.createElement("tbody")
+        body.className = "scheduleBody"
+        for (var i = 0, len = trains.length; i < len; i++) {
+            var t = trains[i];
+            var row = document.createElement("tr");
+            body.appendChild(row);
+
+            var trainName = document.createElement("td");
+            trainName.className = "train";
+            trainName.innerHTML = t.train;
+            row.appendChild(trainName);
+
+            var trainLine = document.createElement("td");
+            trainLine.className = "line";
+            trainLine.innerHTML = t.line;
+            row.appendChild(trainLine);
+
+            var trainArrival = document.createElement("td");
+            trainArrival.className = "arrive";
+            var d = new Date(t.arrive);
+            localTime = d.toLocaleTimeString('en-US', {hour12: this.config.timeFormat == 12}).split(":")
+            trainArrival.innerHTML = localTime[0] + ":" + localTime[1];
+            row.appendChild(trainArrival);
+
+            var trainDelay = document.createElement("td");
+            trainDelay.className = "trainDelay";
+            if (t.delay <= 0) {
+                trainDelay.innerHTML = "On Time";
+            } else {
+                trainDelay.innerHTML = t.delay + " min late";
+            }
+            row.appendChild(trainDelay);
+        }
+        table.appendChild(body);
+        return table;
+    },
+
+    // Creates the delayed train table
+    createDelayTable: function(trains) {
+        var table = document.createElement("table");
+        table.className = "small";
+        for (var i = 0, len = trains.length; i < len; i++) {
+            var t = trains[i];
+            var row = document.createElement("tr");
+            table.appendChild(row);
+
+            var trainName = document.createElement("td");
+            trainName.className = "train";
+            trainName.innerHTML = t.train;
+            row.appendChild(trainName);
+
+            var trainDir = document.createElement("td");
+            trainDir.className = "trainDir";
+            trainDir.innerHTML = t.dir;
+            row.appendChild(trainDir);
+
+            var trainStop = document.createElement("td");
+            trainStop.className = "trainStop";
+            trainStop.innerHTML = t.stop;
+            row.appendChild(trainStop);
+
+            var trainDelay = document.createElement("td");
+            trainDelay.className = "trainDelay";
+            trainDelay.innerHTML = t.delay + " min late";
+            row.appendChild(trainDelay);
+        }
+        return table;
+    },
+
+    // Override dom generator
+    getDom: function() {
+        Log.info("getDom");
+        var wrapper = document.createElement("div");
+
+        if (this.error != null) {
+            // only error we have is a stationName error
+            var error = document.createElement("div");
+            error.innerHTML = "Invalid station name: " + this.config.stationName;
+            var errorMsg = document.createElement("div");
+            errorMsg.className = "small";
+            errorMsg.innerHTML = "You must enter a valid string. Refer to caltrain-stations.txt for a list of valid station names.";
+            wrapper.appendChild(error);
+            wrapper.appendChild(errorMsg);
+            return wrapper;
+        }
+
+        if (!this.loaded) {
+            wrapper.innerHTML = "LOADING";
+            wrapper.className = "dimmed light small";
+            return wrapper;
+        }
+
+        // Generate the delays report
+        if (this.delays.length > 0) {
+            if (this.config.showDelayWarning) {
+                var head = document.createElement("div");
+                head.innerHTML = "Delays Reported";
+                head.className = "warning";
+                wrapper.appendChild(head);
+            }
+
+            if (this.config.showDelayedTrains) {
+                var delayTable = this.createDelayTable(this.delays);
+                wrapper.appendChild(delayTable);
+            }
+        }
+
+        // Generate the station's Northbound train status
+        if (this.stationNorth.length > 0) {
+            var northTable = this.createTrainTable(this.stationNorth);
+            var northHead = document.createElement("div");
+            northHead.className = "scheduleTitle";
+            northHead.innerHTML = "Northbound";
+            wrapper.appendChild(northHead);
+            wrapper.appendChild(northTable);
+        }
+
+        // Generate the station's Southbound train status
+        if (this.stationSouth.length > 0) {
+            var southTable = this.createTrainTable(this.stationSouth);
+            var southHead = document.createElement("div");
+            southHead.className = "scheduleTitle";
+            southHead.innerHTML = "Southbound";
+            wrapper.appendChild(southHead);
+            wrapper.appendChild(southTable);
+        }
+
+        return wrapper;
+    },
+
+    // Override notification handler.
+    socketNotificationReceived: function(query, value) {
+        Log.info("socketNotificationReceived")
+        if (query === "CheckForDelays") {
+            this.delays = value;
+            this.loaded = true;
+            this.updateDom();
+        } else if (query === "GetNorthboundTrains") {
+            this.stationNorth = value;
+            this.loaded = true;
+            this.updateDom();
+        } else if (query === "GetSouthboundTrains") {
+            this.stationSouth = value;
+            this.loaded = true;
+            this.updateDom();
+        } else if (query == "ERROR") {
+            this.error = value;
+            this.updateDom();
+        } else {
+            // Everything else is a debug message
+            Log.info(query);
+            Log.info(value);
+        }
+    },
+});
---
+++ node_helper.js
@@ -0,0 +1,204 @@
+var NodeHelper = require("node_helper")
+var request = require("request")
+var zlib = require("zlib")
+
+const BASE_URL = "http://api.511.org/transit/"
+
+// uncertain if it always holds true that 1 is north and 2 is south
+const STATIONS = {
+    "22nd Street": {"north": 70021, "south": 70022},
+    "Atherton": {"north": 70151, "south": 70152},
+    "Bayshore": {"north": 70031, "south": 70032},
+    "Belmont": {"north": 70121, "south": 70122},
+    "Blossom Hill": {"north": 70291, "south": 70292},
+    "Broadway": {"north": 70071, "south": 70072},
+    "Burlingame": {"north": 70081, "south": 70082},
+    "California Ave": {"north": 70191, "south": 70192},
+    "Capitol": {"north": 70281, "south": 70282},
+    "College Park": {"north": 70251, "south": 70252},
+    "Gilroy": {"north": 70321, "south": 70322},
+    "Hayward Park": {"north": 70101, "south": 70102},
+    "Hillsdale": {"north": 70111, "south": 70112},
+    "Lawrence": {"north": 70231, "south": 70232},
+    "Menlo Park": {"north": 70161, "south": 70162},
+    "Millbrae": {"north": 70061, "south": 70062},
+    "Morgan Hill": {"north": 70301, "south": 70302},
+    "Mountain View": {"north": 70211, "south": 70212},
+    "Palo Alto": {"north": 70171, "south": 70172},
+    "Redwood City": {"north": 70141, "south": 70142},
+    "San Antonio": {"north": 70201, "south": 70202},
+    "San Bruno": {"north": 70051, "south": 70052},
+    "San Carlos": {"north": 70131, "south": 70132},
+    "San Francisco": {"north": 70011, "south": 70012},
+    "San Jose Diridon": {"north": 70261, "south": 70262},
+    "San Martin": {"north": 70311, "south": 70312},
+    "San Mateo": {"north": 70091, "south": 70092},
+    "Santa Clara": {"north": 70241, "south": 70242},
+    "South San Francisco": {"north": 70041, "south": 70042},
+    "Sunnyvale": {"north": 70221, "south": 70222},
+    "Tamien": {"north": 70271, "south": 70272},
+    // "Tamien-other": {"north": 777403},
+    // "San Jose": {"north": 777401},
+}
+
+module.exports = NodeHelper.create({
+    start: function() {
+        console.log("Starting node helper: " + this.name);
+        this.sendSocketNotification("starting node helper", "");
+    },
+
+    socketNotificationReceived: function(query, parameters) {
+        var self = this;
+        console.log("Query: " + query + " Parameters: " + parameters);
+        var options;
+        if(query === "CheckForDelays") {
+            options = self.buildCheckForDelays(parameters);
+        } else if(query === "GetStationStatus") {
+            var station = STATIONS[parameters.stationName];
+            if (station == null) {
+                self.sendSocketNotification("ERROR", "stationName");
+                return;
+            }
+            options = self.buildGetStationStatus(parameters, station[parameters.trainDirection]);
+        }
+
+        request(options, function(err, resp, body) {
+            if(!err && resp.statusCode == 200) {
+                if(resp.headers["content-encoding"] == "gzip") {
+                    zlib.gunzip(body, function(err, result) {
+                        if (err) {
+                            console.log("Error gunzipping: ", err);
+                        } else {
+                            self.requestHandler(query, parameters, result.toString("utf-8").trim());
+                        }
+                    })
+                }
+            } else {
+                self.sendSocketNotification("request error", resp);
+            }
+        })
+    },
+
+    requestHandler: function(query, parameters, data) {
+        if(query === "CheckForDelays") {
+            options = this.checkForDelaysCallback(data, parameters.delayThreshold);
+        } else if(query === "GetStationStatus") {
+            options = this.getStationStatusCallback(data, parameters.trainDirection);
+        }
+    },
+
+    // compare the aimed arrival and expected arrival times to find delays
+    checkForDelaysCallback: function(data, delayThreshold) {
+        var self = this;
+        var delayedTrains = [];
+        var json = JSON.parse(data);
+        var data = json.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit;
+        for (var i = 0, len = data.length; i < len; i++) {
+            var train = data[i].MonitoredVehicleJourney;
+            var call = train.MonitoredCall;
+            var delay = self.getDelay(call);
+            // Sometimes the api doesn't populate train.VehicleRef
+            var trainRef = train.FramedVehicleJourneyRef.DatedVehicleJourneyRef;
+
+            if (delay > delayThreshold) {
+                delayedTrains.push({
+                    train: train.FramedVehicleJourneyRef.DatedVehicleJourneyRef,
+                    stop: call.StopPointName.split(" Caltrain")[0],
+                    dir: train.DirectionRef,
+                    delay: Math.round(delay / 1000 / 60),
+                });
+            }
+        }
+        console.log("CheckForDelaysCallback");
+        self.sendSocketNotification("CheckForDelays", delayedTrains);
+    },
+
+    // returns all reported train statuses for a given station
+    getStationStatusCallback: function(data, direction) {
+        var self = this;
+        stationStatus = [];
+        var json = JSON.parse(data);
+        var data = json.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit;
+        for (var i = 0, len = data.length; i < len; i++) {
+            var train = data[i].MonitoredVehicleJourney;
+            var call = train.MonitoredCall;
+            var delay = self.getDelay(call);
+            var status = Math.round(delay / 1000 / 60);
+            // Sometimes the api doesn't populate train.VehicleRef
+            var trainRef = train.FramedVehicleJourneyRef.DatedVehicleJourneyRef;
+
+            stationStatus.push({
+                train: trainRef,
+                delay: status,
+                dir: train.DirectionRef,
+                line: train.LineRef,
+                arrive: call.AimedArrivalTime,
+            });
+        }
+
+        console.log("GetStationStatusCallback");
+        if (direction === "south") {
+            self.sendSocketNotification("GetSouthboundTrains", stationStatus);
+        } else if (direction === "north") {
+            self.sendSocketNotification("GetNorthboundTrains", stationStatus);
+        }
+    },
+
+    getDelay: function(call) {
+        /* The caltrain API can do some stupid stuff. For instance:
+        {
+            "AimedArrivalTime": "2019-10-05T00:05:00Z",
+            "ExpectedArrivalTime": "2019-10-05T23:48:46Z",
+            "AimedDepartureTime": "2019-10-06T00:05:00Z",
+            "ExpectedDepartureTime": "2019-10-06T00:05:00Z",
+        }
+
+        In this case, the AimedArrivalTime didn't increment the day when it
+        went over 23:59, so now we have to deal with that.
+        */
+        var arrival = Date.parse(call.AimedArrivalTime);
+        var exp = Date.parse(call.ExpectedArrivalTime);
+        var now = Date.now();
+
+        // The API can mess up the aimed arrival time. If the arrival time is
+        // earlier than the current time, use the ExpectedDepartureTime
+        if (arrival < now) {
+            arrival = Date.parse(call.AimedDepartureTime);
+        }
+
+        return exp - arrival
+    },
+
+    // returns the request parameters to get delayed train status
+    buildCheckForDelays: function(parameters) {
+        return {
+            url: BASE_URL + "StopMonitoring",
+            method: "GET",
+            encoding: null,
+            qs: {
+                "agency": "CT",
+                "api_key": parameters.key,
+            },
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+    },
+
+    // returns the request parameters to get station train status
+    buildGetStationStatus: function(parameters, stopCode) {
+        return {
+            url: BASE_URL + "StopMonitoring",
+            method: "GET",
+            encoding: null,
+            qs: {
+                "agency": "CT",
+                "stopCode": stopCode,
+                "api_key": parameters.key,
+            },
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+    },
+})
